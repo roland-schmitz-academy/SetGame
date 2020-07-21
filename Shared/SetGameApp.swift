ed13305293bdb0603279d64cc46627743a65a2a6
Roland Schmitz <rola@2rs.de>
2020-07-22 00:04:53 +0200

    Initial Commit

---
+++ Shared/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  Shared
+//
+//  Created by Roland Schmitz on 22.07.20.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
